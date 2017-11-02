0ac172a5325665c385a591c2b24240bde97b0b62
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2017-11-02 16:52:31 +0800

    added bootstrap vue

--- src/common/header.vue
+++ src/common/header.vue
@@ -1,6 +1,13 @@
 <template lang="html">
   <div class="">
     <h1>header</h1>
+    <div class="pull-right">
+      <b-nav>
+        <b-nav-item to="/" active>Active</b-nav-item>
+        <b-nav-item to="/about">Link</b-nav-item>
+        <b-nav-item to="example">Another Link</b-nav-item>
+      </b-nav>
+    </div>
   </div>
 </template>
 
