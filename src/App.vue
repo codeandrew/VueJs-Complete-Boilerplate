17fc4f7238ae576390375f75a315c9942bdc00a4
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2017-11-02 16:39:58 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <head-component/>
+      <img src="./assets/logo.png">
+      <router-view/>
+    <foot-component/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+<style lang="scss">
+   @import './scss/main.scss';
+</style>
---
+++ src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+//======================
+// Components
+import header from './common/header'
+import footer from './common/footer'
+
+Vue.component('head-component', header )
+Vue.component('foot-component', footer )
+
+
+
+//==================
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
