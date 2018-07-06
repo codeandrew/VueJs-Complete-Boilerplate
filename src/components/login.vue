59bae0057a7b9980819b13465680b23d170ac6e6
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-07-06 15:51:03 +0800

    Create Login component

---
+++ src/components/login.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div class="login">
+    <h3>Sign In</h3>
+    <input type="text" name="" value="" placeholder="Email">
+    <input type="password" name="" value="" placeholder="Password">
+    <button type="button" name="button"></button>
+    <p>You don't have account ? You can create one</p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  // name : login,
+  data(){
+    return { }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
