f55860ecea2ac906a721080d8c639091a84f7b7c
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-07-06 22:13:28 +0800

    Create SignUp Vue

---
+++ src/components/SignUp.vue
@@ -0,0 +1,49 @@
+<template lang="html">
+  <div class="sign-up">
+    <p> {{ subheader }}</p>
+    <input type="text" placeholder="Email">
+    <input type="password" placeholder="Password">
+    <b-button  variant="success">
+      Sign Up
+    </b-button>
+    <span> or go back to login</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      subheader : "Let's Create a new account !"
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+.sign-up {
+  margin-top : 40px;
+
+  input {
+    margin : 10px auto;
+    width : 300px;
+    padding : 15px;
+    display : block;
+  }
+
+  button {
+    margin-top : 10px;
+    width: 150px;
+    cursor: pointer;
+  }
+}
+
+span {
+  display : block;
+  margin-top : 20px;
+  font-size : 11px;
+}
+
+
+</style>
