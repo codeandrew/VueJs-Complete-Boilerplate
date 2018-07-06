2e27f3c5b41ee6988150888c73d1c5777fc5397a
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-07-06 21:51:48 +0800

    Add style in login.vue

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,9 +1,13 @@
 <template lang="html">
   <div class="login">
+    <img src="../assets/logo.png">
     <h3>Sign In</h3>
     <input type="text" name="" value="" placeholder="Email">
     <input type="password" name="" value="" placeholder="Password">
-    <button type="button" name="button"></button>
+
+    <b-button variant="success" name="button">
+      Sign In
+    </b-button>
     <p>You don't have account ? You can create one</p>
 
   </div>
@@ -18,5 +22,32 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="scss">
+  .login {
+    margin-top : 40px;
+
+    input {
+      margin : 10px auto;
+      width : 300px;
+      padding : 15px;
+      display : block;
+    }
+
+  }
+  button {
+    margin-top : 20px;
+    width: 150px;
+    cursor: pointer;
+  }
+  p {
+    margin-top: 40px;
+    font-size : 13px;
+    a {
+      text-decoration : underline;
+      cursor : pointer;
+    }
+  }
+
+
+
 </style>
