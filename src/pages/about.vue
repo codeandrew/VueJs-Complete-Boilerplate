e634bb1c1021375fd162e78331f41cc97d0e9846
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-07-06 14:50:27 +0800

    Implement VuexStore in the aboutPage

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -3,26 +3,29 @@
     <b-container>
     <h1>About Page</h1>
     <br />
-      <p> This is a boiler Template of VueJs to reduce time in setting up and more on actual coding</p>
-      <p> together with: </p>
+      <p v-for="item in about"> {{ item }} </p>
       <ul class='about-list'>
-        <li v-for="item in dep"> {{ item }}</li>
+        <li v-for="item in dependencies"> {{ item }}</li>
       </ul>
     </b-container>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   data() {
-    return {
-      dep : [
-        'BootstrapVue',        
-        'VueRouter',
-        'Vuex',
-        'Sass/Scss'
-      ]
-    }
+    about : [
+      'This is a boiler Template of VueJs to reduce time in setting up and more on actual coding',
+      'together with: '
+    ]
+  },
+
+  computed : {
+    ...mapState([
+      'dependencies'
+    ])
   }
 }
 </script>
