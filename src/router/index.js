2d18e0c8cf7b4df2686d1da066e1327c39b1a0a3
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-07-09 16:48:09 +0800

    Comment elseIf in Router Guard

--- src/router/index.js
+++ src/router/index.js
@@ -55,7 +55,7 @@ router.beforeEach( (to, from, next) => {
   let requiresAuth = to.matched.some( record => record.meta.requiresAuth )
 
   if ( requiresAuth && !currentUser ) next('Login')
-  else if ( !requiresAuth && !currentUser) next('Hello')
+  // else if ( !requiresAuth && !currentUser) next('Hello')
   else next()
 })
 
