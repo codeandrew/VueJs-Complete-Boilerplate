57f4be4d78e552af53544e702ad52106bcd56bbf
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-07-13 20:06:34 +0800

    Create add to-do form

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -2,6 +2,42 @@
   <div class="to-do">
     <h2> To Do </h2>
     <b-container>
+      <div class="add-todo">
+          <b-form @submit="submit">
+            <b-form-group
+            label="Action" >
+              <b-form-input
+              v-model="form.action"
+              placeholder="action"
+              />
+            </b-form-group>
+
+            <b-form-group
+            label="Description" >
+              <b-form-input
+              v-model="form.description"
+              placeholder="description"
+              />
+            </b-form-group>
+
+            <b-form-group
+            label="Remarks" >
+              <b-form-input
+              v-model="form.remarks"
+              placeholder="remarks"
+              />
+            </b-form-group>
+
+            <b-button
+            type="submit"
+            variant="primary"
+             >
+              Submit
+            </b-button>
+
+          </b-form>
+      </div>
+
       <b-table striped hover :items="todo"></b-table>
     </b-container>
   </div>
