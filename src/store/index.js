319305ed71f23e4c9d475b8e489b59dff1864524
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-07-15 18:42:13 +0800

    Implement vuex-persist, create vuexLocalStorage
    store data in localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,9 @@ import Vuex from 'vuex'
 import axios from 'axios'
 import firebase from 'firebase'
 import router from '../router'
+import VuexPersist from 'vuex-persist'
+import * as Cookies from 'js-cookie'
+
 
 Vue.use(Vuex);
 
@@ -17,7 +20,19 @@ let baseData = {
   ]
  }
 
+
+ const vuexLocalStorage = new VuexPersist({
+   key: 'vuex', // The key to store the state on in the storage provider.
+   storage: window.localStorage, // or window.sessionStorage or localForage
+   // Function that passes the state and returns the state with only the objects you want to store.
+   // reducer: state => state,
+   // Function that passes a mutation and lets you decide if it should update the state in localStorage.
+   // filter: mutation => (true)
+ })
+
+
 export const store = new Vuex.Store({
+
     state : {
       dependencies : { ...baseData.dependencies } ,
       routes : [ ...router.options.routes ],
@@ -144,6 +159,10 @@ export const store = new Vuex.Store({
 
     modules: {
 
-    }
+    },
+
+    plugins: [
+     vuexLocalStorage.plugin
+   ]
 
 });
