3d05b55716ab521ec219545c9d08cbdb66f903b4
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-07-24 21:30:24 +0800

    Move firebase credentials on dev.env.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,17 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  ROOT_API: '"http://localhost/api"',
+  FIREBASE:  `
+  {
+    apiKey: "AIzaSyB8PthQVcXILdk398_PcpBb6lOJ18obofE",
+    authDomain: "to-do-app-876b4.firebaseapp.com",
+    databaseURL: "https://to-do-app-876b4.firebaseio.com",
+    projectId: "to-do-app-876b4",
+    storageBucket: "to-do-app-876b4.appspot.com",
+    messagingSenderId: "600361348663"
+  }
+  `
+
 })
